b9df5818adf3f1c4c2154f98a6447ed9bb6ba3c1
Rizki Arsyad <arsyadrizki16@gmail.com>
2026-09-12 16:12:38 +0700

    hum 1.3.3

--- Casks/hum.rb
+++ Casks/hum.rb
@@ -1,6 +1,6 @@
 cask "hum" do
-  version "1.3.2"
-  sha256 "d5468652de6023f2ee800b9b47e648c8ef1434357db477007f4336ce25ba9cd6"
+  version "1.3.3"
+  sha256 "93c08ea1b8da051715ac47eacac9b85185352a880f6d7a48b821a64194f1303b"
 
   url "https://github.com/rzkarsyad/Hum/releases/download/v#{version}/Hum-#{version}.dmg"
   name "Hum"
